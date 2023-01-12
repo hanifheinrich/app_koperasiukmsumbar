7b53dff56d5f0f75a381a2dc1d261b69e64f7fb8
dzulaji <dzulfauzi2@gmail.com>
2023-01-12 18:51:47 +0700

    commit aji

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('user.main');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
 });
 
 Route::get('/dashboard-admin', function () {
