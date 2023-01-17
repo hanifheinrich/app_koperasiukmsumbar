c79f9b81e4cfd34ed8c550fa69599cc2733f0074
dzulaji <dzulfauzi2@gmail.com>
2023-01-17 12:01:33 +0700

    commit aji

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,7 +36,7 @@
               <div class="card" style="border-radius: 1rem;">
                 <div class="row g-0">
                   <div class="col-md-6 col-lg-5 d-none d-md-block">
-                    <img src="assets/images/IMG_6151.png"
+                    <img src="assets/images/IMG_20230113_130910.jpg"
                       alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
                   </div>
                   <div class="col-md-6 col-lg-7 d-flex align-items-center">
